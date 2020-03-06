7620c042341971123bb3a2edf9be3dd8c0ac23af
etovar1989 <etovar1989@gmail.com>
2020-03-06 17:21:15 -0500

    Dia 1

---
+++ app/src/main/java/com/example/eventoeduteka/dia1QR.java
@@ -0,0 +1,56 @@
+package com.example.eventoeduteka;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class dia1QR extends AppCompatActivity implements ZXingScannerView.ResultHandler{
+
+    private ZXingScannerView myScannerView;
+
+    String documento;
+    String tipo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_dia1_q_r );
+
+        myScannerView = new ZXingScannerView( this );
+        setContentView( myScannerView );
+        myScannerView.setResultHandler( this );
+        myScannerView.startCamera();
+
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult( requestCode, permissions, grantResults );
+
+    }
+
+
+    @Override
+    public void handleResult(Result result) {
+        documento = result.getText();
+        tipo = getIntent().getStringExtra( "tipo" );
+
+        Toast.makeText( this,"Consulta "+documento, Toast.LENGTH_SHORT).show();
+
+        Intent in = new Intent( dia1QR.this, dia1Resultado.class );
+
+        in.putExtra( "documento",documento);
+        in.putExtra( "tipo",tipo);
+
+        startActivity( in );
+        finish();
+    }
+
+}
