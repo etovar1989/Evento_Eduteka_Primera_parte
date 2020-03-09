e2dd64a4fc9b28b02e54ecbf6c47f82fc2cd6eeb
etovar1989 <etovar1989@gmail.com>
2020-03-09 11:31:45 -0500

    Dia 2

--- app/src/main/java/com/example/eventoeduteka/dia2QR.java
+++ app/src/main/java/com/example/eventoeduteka/dia2QR.java
@@ -2,13 +2,56 @@ package com.example.eventoeduteka;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class dia2QR extends AppCompatActivity {
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class dia2QR extends AppCompatActivity implements ZXingScannerView.ResultHandler{
+
+    private ZXingScannerView myScannerView;
+
+    String documento,tipo,id;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_dia2_q_r );
+
+        myScannerView = new ZXingScannerView( this );
+        setContentView( myScannerView );
+        myScannerView.setResultHandler( this );
+        myScannerView.startCamera();
+
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult( requestCode, permissions, grantResults );
+
+    }
+
+
+    @Override
+    public void handleResult(Result result) {
+        documento = result.getText();
+        tipo = getIntent().getStringExtra( "tipo" );
+
+        Toast.makeText( this,"Consulta "+documento, Toast.LENGTH_SHORT).show();
+
+        Intent in = new Intent( dia2QR.this, dia2Resultado.class );
+
+        in.putExtra( "documento",documento);
+        in.putExtra( "tipo",tipo);
+        in.putExtra( "id",id);
+
+        startActivity( in );
+        finish();
+    }
+
+
 }
